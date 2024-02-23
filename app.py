0be7abf329617ae3e114570d417446571a095fa7
RameesMhd <muhammed.ramees@neopraxis.in>
2024-02-23 14:37:14 +0530

    new count added

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
 import os
 from flask import Flask, render_template, request
 import pytube
-
+from pytube import YouTube
 app = Flask(__name__)
 
 def download_playlist(playlist_url, output_path=os.path.join(os.path.expanduser("~"), "Downloads/YouTube")):
     playlist = pytube.Playlist(playlist_url)
-    print(f"Number of videos in playlist: {len(playlist.videos)}")
 
-    count = 1
+    print(f"\n{'*'*40}\n*** Number of videos in playlist: {len(playlist.videos)} ***\n{'*'*40}")
+
+    count = 0
     for video in playlist.videos:
         try:
             audio_stream = video.streams.filter(only_audio=True).first()
             output_filename = f"{os.path.splitext(audio_stream.default_filename)[0]}.mp3"
             output_filepath = os.path.join(output_path, output_filename)
             audio_stream.download(output_path=output_path, filename=output_filename)
+            count += 1
             print(f"{count} Downloaded: {output_filename}")
         except Exception as e:
             print(f"Error downloading {video.title}: {e}")
