c0e2541d080114ed2f1700d12830104117cb851e
RameesMhd <muhammed.ramees@neopraxis.in>
2024-03-05 15:52:26 +0530

    Now delete the video file after converting to mp3 format.

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
 import os
-from flask import Flask, render_template, request
-from pytube import Playlist
-from pytube import YouTube
-from moviepy.editor import VideoFileClip
 import re
+from flask import Flask, render_template, request
+from pytube import Playlist, YouTube
+from moviepy.editor import VideoFileClip
 
 app = Flask(__name__)
 
 def clean_filename(filename):
-    # Remove non-English characters
+    # Replace all characters other than English letters with space
     cleaned_filename = re.sub(r'[^a-zA-Z]+', ' ', filename)
     return cleaned_filename
+
+def convert_to_mp3(input_file, output_file):
+    try:
+        video = VideoFileClip(input_file)
+        audio = video.audio
+        audio.write_audiofile(output_file, codec='mp3')
+        print("Conversion completed successfully.")
+    except Exception as e:
+        print(f"Error converting video to MP3: {e}")
+    finally:
+        video.close()  # Close the video clip after conversion
 
 def download_playlist(playlist_url, output_path=os.path.join(os.path.expanduser("~"), "Downloads/YouTube")):
     playlist = Playlist(playlist_url)
@@ -22,24 +32,23 @@ def download_playlist(playlist_url, output_path=os.path.join(os.path.expanduser(
         try:
             video = YouTube(video_url)
             video_stream = video.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first() or video.streams.filter(file_extension='mp4').order_by('resolution').desc().first()
-            audio_stream = video.streams.filter(only_audio=True, file_extension='mp4').order_by('abr').desc().first()
+            # audio_stream = video.streams.filter(only_audio=True, file_extension='mp3').order_by('abr').desc().first()
             
-            if video_stream and audio_stream:
-                # Clean and truncate the filename
+            if video_stream:
                 video_title = clean_filename(video.title)
                 video_filename = f"{video_title}.mp4"
-                audio_filename = f"{video_title}_audio.mp4"
-                
-                # Check if filename is empty
-                if not video_title:
-                    video_filename = f"songs_{count}.mp4"
-                    audio_filename = f"songs_{count}_audio.mp4"
-                
+                audio_filename = f"{video_title}.mp3"
+
                 video_stream.download(output_path=output_path, filename=video_filename)
-                audio_stream.download(output_path=output_path, filename=audio_filename)
-                count += 1
-                percentage_complete = (count / total_videos) * 100
-                print(f"{count}/{total_videos} ({percentage_complete:.2f}%) Downloaded: {video.title}")
+                convert_to_mp3(os.path.join(output_path, video_filename), os.path.join(output_path, audio_filename))
+
+                # Try deleting the video file only if conversion is successful
+                try:
+                    os.remove(os.path.join(output_path, video_filename))
+                    print(f"Deleted video file: {video_filename}")
+                except OSError as e:
+                    print(f"Error deleting video file: {video_filename} - {e}")
+            
             else:
                 print(f"No suitable streams found for {video.title}")
         except Exception as e:
